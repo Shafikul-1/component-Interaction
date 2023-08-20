2b0393c6f440a2e5bc3503081577a2d9939dffe8
Shafikul <shafikul.18288@gmail.com>
2023-08-20 07:45:21 +0600

    commit

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, reactive } from 'vue'
+import ParentComponent from './ParentComponent.vue';
+const massage = 'Massage Test' 
+const EmitDefine = reactive({
+    msg: 'Define Emit Check'
+})
+const buttonMassage = ref('Button Massage')
+function buttonClick(){
+    buttonMassage.value = 'Clicked Button & UPdate Massage'
+}
+</script>
+<template>
+    
+    <p>{{ EmitDefine.msg }} </p>
+    
+    
+    <ParentComponent v-model:messageEmitted ="EmitDefine.msg" :massage="massage" :eventHendelar="buttonClick"/> 
+    <p>{{ buttonMassage }}</p>
+</template>.
+
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,14 @@
+
+<script setup>
+defineProps(['messageEmitted', 'massage', 'eventHendelar' ]);
+defineEmits(['update:messageEmitted '])
+</script> 
+<template>
+    <div>
+        <h1 class="text-4xl font-bold text-center my-6">{{ massage }}</h1>
+
+        <input @input="$emit('update:messageEmitted', $event.target.value)" type="text" :value="messageEmitted" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+
+        <button @click="eventHendelar()" class="my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Click Here </button>
+    </div>
+</template>
